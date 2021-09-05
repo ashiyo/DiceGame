49ce108b9b98ae17dc0435ee5f9a451477391317
harshita <harshitamreddy@gmail.com>
2021-09-04 19:50:15 -0700

    First Commit

---
+++ Dicee Game/index.js
@@ -0,0 +1,21 @@
+// Generate random number for image 1
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomDiceImage = "dice" + randomNumber1 +".png";
+var randomImageSource = "images/"+randomDiceImage;
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource);
+// Generate random number for image 2
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomImageSource2 = "images/dice"+randomNumber2+".png";
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);
+// Display who won
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML="Player 1 Wins!"
+}
+else if (randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML="Player 2 Wins!"
+}
+else {
+  document.querySelector("h1").innerHTML="Draw!"
+}
